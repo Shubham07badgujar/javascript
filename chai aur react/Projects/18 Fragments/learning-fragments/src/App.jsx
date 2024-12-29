b700712d91eaa854892256823dbf5782a32c3eab
Shubham07badgujar <shubhambadgujari076@gmail.com>
2024-12-29 10:48:04 +0530

    update

--- chai aur react/Projects/18 Fragments/learning-fragments/src/App.jsx
+++ chai aur react/Projects/18 Fragments/learning-fragments/src/App.jsx
@@ -1,16 +1,21 @@
+import React from 'react'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
-  
+   let myArray = ['Dal','GGreen Vegetable', 'Roti', 'Salad','Milk','ghee']
 
   return (
-    <div>
-     Fragments
-    
-    </div>
+    <React.Fragment>
+      <div>Healthy Food</div>
+        <ul className="list-group">
+         {myArray.map((item) => <li key={item} class="list-group-item">{item}</li>)}
+          
+        </ul>
+        </React.Fragment>
   )
 }
 
