88043945fdfd8570eb0003e46041d34ffedad6bd
Shubham07badgujar <shubhambadgujari076@gmail.com>
2024-12-31 23:23:00 +0530

    update

--- chai aur react/Projects/18 Fragments/learning-fragments/src/App.jsx
+++ chai aur react/Projects/18 Fragments/learning-fragments/src/App.jsx
@@ -1,21 +1,30 @@
-import React from 'react'
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import ErrorMessage from './component/ErrorMessage'
-import FoodItem from './component/FoodItem'
-import Container from './component/Container'
+import React from "react";
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import ErrorMessage from "./component/ErrorMessage";
+import FoodItem from "./component/FoodItem";
+import Container from "./component/Container";
 
 function App() {
-   let foodItem = ['Dal','GGreen Vegetable', 'Roti', 'Salad','Milk','ghee']
+  let foodItem = ["Dal", "GGreen Vegetable", "Roti", "Salad", "Milk", "ghee"];
 
   return (
-    <Container>
-      <div className='food-heading'>Healthy Food</div>
-      <FoodItem item={foodItem}></FoodItem>
-      <ErrorMessage item={foodItem} ></ErrorMessage>        
-    </Container>
+    <>
+      <Container>
+        <div className="food-heading">Healthy Food</div>
+        <FoodItem item={foodItem}></FoodItem>
+        <ErrorMessage item={foodItem}></ErrorMessage>
+      </Container>
+
+      <Container>
+        <p>
+          Above is the list of healthy that are good for your health and well
+          being
+        </p>
+      </Container>
+    </>
   );
 }
 
--- chai aur react/Projects/18 Fragments/learning-fragments/src/component/Container.jsx
+++ chai aur react/Projects/18 Fragments/learning-fragments/src/component/Container.jsx
@@ -1,7 +1,7 @@
 import styles from './Container.module.css'
 
 const Container = (props) => {
-    return <div className={styles.Container}>{props.children}</div>
+    return <div className={styles.container}>{props.children}</div>
     
 };
 
