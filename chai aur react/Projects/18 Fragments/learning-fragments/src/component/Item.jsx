d934b446b490beb19259e07d24db4f4eaa14f7e8
Shubham07badgujar <shubhambadgujari076@gmail.com>
2024-12-31 23:55:59 +0530

    guug

--- chai aur react/Projects/18 Fragments/learning-fragments/src/component/Item.jsx
+++ chai aur react/Projects/18 Fragments/learning-fragments/src/component/Item.jsx
@@ -1,11 +1,17 @@
 import FoodItem from "./FoodItem";
-import styles from "./Item.module.css"
-const Item = ({foodItem}) => {
-     return (
-     <li 
-     className="{styles['kg-item']} list-group-item">
-     <span className={styles['kg-span']}>{foodItem}</span>
-     </li>);
-}
+import styles from "./Item.module.css";
+const Item = ({ foodItem }) => {
+  return (
+    <li className="{styles['kg-item']} list-group-item">
+      <span className={styles["kg-span"]}>{foodItem}</span>
+      <button
+        className={`${styles.button} btn btn-info`}
+        onClick={() => console.log(`${fooditem} being bought`)}
+      >
+        Buy
+      </button>
+    </li>
+  );
+};
 
 export default Item;
