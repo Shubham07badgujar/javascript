ab87d7a218fa50f921fc50e1cedb9a9916dd4172
Shubham07badgujar <shubhambadgujari076@gmail.com>
2025-01-01 22:38:10 +0530

    change

--- chai aur react/Projects/18 Fragments/learning-fragments/src/App.jsx
+++ chai aur react/Projects/18 Fragments/learning-fragments/src/App.jsx
@@ -6,16 +6,39 @@ import "./App.css";
 import ErrorMessage from "./component/ErrorMessage";
 import FoodItem from "./component/FoodItem";
 import Container from "./component/Container";
+import FoodInput from "./component/FoodInput";
 
 function App() {
-  let foodItem = ["Dal", "GGreen Vegetable", "Roti", "Salad", "Milk", "ghee"];
+  // let foodItem = ["Dal", "GGreen Vegetable", "Roti", "Salad", "Milk", "ghee"];
+
+  // let textState = useState("Food Item Entered by User");
+  // let textToShow = textState[0];
+  // let setTextState = textState[1]
+
+  // console.log(`Cureent Value of textState is : ${textToShow}`);
+
+  let [textToShow, setTextState] = useState();
+  let [foodItem, setFoodItems] = useState(["Dal", "Green Vegetable", "Roti"]);
+
+  const onKeyDown = (event) => {
+    if(event.key === 'Enter') {
+      let newFoodItem = event.target.value
+      let newAdd =[...foodItem,newFoodItem]
+      setFoodItems(newAdd)
+      console.log("food Value is " + newFoodItem);
+      
+    }
+  };
 
   return (
     <>
       <Container>
         <div className="food-heading">Healthy Food</div>
-        <FoodItem item={foodItem}></FoodItem>
         <ErrorMessage item={foodItem}></ErrorMessage>
+        <FoodInput handleKeyDown={onKeyDown}></FoodInput>
+        <p>{textToShow}</p>
+
+        <FoodItem item={foodItem}></FoodItem>
       </Container>
 
       <Container>
