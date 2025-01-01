e0f5420f5bf42deebf494b5d344256bbfa91e5cf
Shubham07badgujar <shubhambadgujari076@gmail.com>
2025-01-01 22:39:15 +0530

    11

--- chai aur react/Projects/18 Fragments/learning-fragments/src/component/Item.jsx
+++ chai aur react/Projects/18 Fragments/learning-fragments/src/component/Item.jsx
@@ -6,7 +6,7 @@ const Item = ({ foodItem }) => {
       <span className={styles["kg-span"]}>{foodItem}</span>
       <button
         className={`${styles.button} btn btn-info`}
-        onClick={() => console.log(`${fooditem} being bought`)}
+        onClick={() => console.log(`${foodItem} being bought`)}
       >
         Buy
       </button>
