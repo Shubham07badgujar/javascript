0b78dd9ba8bbe7dfc0962548cbd50a2f148a28c3
Shubham07badgujar <shubhambadgujari076@gmail.com>
2025-01-19 23:41:30 +0530

    let 2

--- Node-Tut/Lec 11/index.js
+++ Node-Tut/Lec 11/index.js
@@ -3,4 +3,5 @@ const http = require('http');
 http.createServer((request, response)=>{
     response.writeHead(200,{'content-type':'application/JSON'});
     response.write({name:'shubham',mail:'shubham@.com'})
+    response.end()
 })
