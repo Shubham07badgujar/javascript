650aa78bc32b007754b0485c5c9b86d24c35eccf
Shubham07badgujar <shubhambadgujari076@gmail.com>
2025-01-21 22:10:24 +0530

    l

---
+++ Node-Tut/Code Step/18-lec/index.js
@@ -0,0 +1,21 @@
+// what is expressjs
+// install Express
+// Make Example with expressjs
+
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("hello , THis is home page");
+});
+
+app.get("/about", (req, res) => {
+  res.send("hello , THis is about page");
+});
+
+app.get("/help", (req, res) => {
+  res.send("hello , THis is help page");
+});
+
+app.listen(3000);
