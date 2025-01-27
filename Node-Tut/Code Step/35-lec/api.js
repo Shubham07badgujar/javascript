51137712edaa62631293c7eefe094eddca9aad01
Shubham07badgujar <shubhambadgujari076@gmail.com>
2025-01-27 20:02:07 +0530

    node

--- Node-Tut/Code Step/35-lec/api.js
+++ Node-Tut/Code Step/35-lec/api.js
@@ -1,21 +1,33 @@
-const express = require('express');
-const dbConnect = require('./mongodb');
-const { name } = require('ejs');
+const express = require("express");
+const dbConnect = require("./mongodb");
+const { name } = require("ejs");
 const app = express();
 
-app.get('/',async (req, resp)=>{
-    let data = await dbConnect();
-    data = await data.find().toArray();
-    console.log(data);
-    resp.send(data)
-    
+app.use(express.json());
+
+app.get("/", async (req, resp) => {
+  let data = await dbConnect();
+  data = await data.find().toArray();
+  // console.log(data);
+  resp.send(data);
 });
 
-app.post('/', async (req, resp)=>{
-    let data = await dbConnect();
-    // data = await data.insertOne({name: 'Ali', age: 30});
-    // console.log(data);
-    resp.send({name: 'Ali', age: 30});
-    
+app.post("/", async (req, resp) => {
+  let data = await dbConnect();
+  let result = await data.insertOne(req.body);
+  // console.log(result);
+  // resp.send({name: 'Ali', age: 30});
+  resp.send(result);
+});
+
+app.put("/", async (req, resp) => {
+  // console.log(req.body);
+  let data = await dbConnect();
+  let result = await data.updateOne(
+    { name: req.body.name },    
+    { $set:req.body }
+  );
+
+  resp.send({ result: "Put Method" });
 });
 app.listen(3000);
