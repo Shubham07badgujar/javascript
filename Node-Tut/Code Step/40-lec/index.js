d057ef518ff25aa4a2ab318aad8fc0a1f6cbb88f
Shubham07badgujar <shubhambadgujari076@gmail.com>
2025-01-28 11:51:28 +0530

    node

--- Node-Tut/Code Step/40-lec/index.js
+++ Node-Tut/Code Step/40-lec/index.js
@@ -9,10 +9,10 @@ const productSchema = new mongoose.Schema({
 
 });
 
-const main = async () => {
+const saveInDb = async () => {
     const Product = mongoose.model('products', productSchema);
     let data = new Product({
-        namae: "max 132",
+        name: "max 132",
         price: 200, 
         brand: "maxx",
         category: "phone"
@@ -21,4 +21,30 @@ const main = async () => {
     console.log(result);
 
 }
-main();
+
+const updateInDb = async () =>{
+    const Product = mongoose.model('products', productSchema) ;
+    let data = await Product.updateOne(
+        {name:"max 132"},{
+            $set:{price:40000}
+        }
+    );
+    console.log(data);
+    
+}
+
+const deleteInDb = async () =>{
+    const Product = mongoose.model('products', productSchema);
+    let data = await Product.deleteOne({name:"m8"});
+    console.log(data);
+    
+}
+
+const findInDb = async () =>{
+    const Product = mongoose.model('products', productSchema);
+    let data = await Product.find({name:"Note pro"});
+    console.log(data);
+    
+}
+
+findInDb();
