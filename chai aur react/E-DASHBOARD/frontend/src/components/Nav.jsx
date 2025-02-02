89a755fa0bb7e0aff07487ad0cee469b6c353c63
Shubham07badgujar <shubhambadgujari076@gmail.com>
2025-02-02 23:43:50 +0530

    node

--- chai aur react/E-DASHBOARD/frontend/src/components/Nav.jsx
+++ chai aur react/E-DASHBOARD/frontend/src/components/Nav.jsx
@@ -8,9 +8,10 @@ const Nav = () => {
       <ul className='nav-ul'>
         <li><Link to="/">Product</Link></li>
         <li><Link to="/add">Add product</Link></li>
-        <li><Link to="/update">update product</Link></li>
+        <li><Link to="/update">Update product</Link></li>
         <li><Link to="/logout">Logout</Link></li>
-        <li><Link to="/profile">profile</Link></li>
+        <li><Link to="/profile">Profile</Link></li>
+        <li><Link to="/signup">SignUp</Link></li>
 
       </ul>
     </div>
