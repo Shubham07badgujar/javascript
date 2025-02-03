a03f2a94e87c5e176c6ec8bb0dec71c53e11ed03
Shubham07badgujar <shubhambadgujari076@gmail.com>
2025-02-03 16:31:02 +0530

    node

--- chai aur react/E-DASHBOARD/frontend/src/components/SignUp.jsx
+++ chai aur react/E-DASHBOARD/frontend/src/components/SignUp.jsx
@@ -1,9 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import "./SignUp.css";
 
-const SignUp = () =>{
-    return(
-        <div>signUp</div>
-    )
-}
+const SignUp = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const collectData = () => {
+    console.warn("Name: ", name);
+    console.warn("Email: ", email);
+    console.warn("Password: ", password);
+  };
+
+  return (
+    <div className="signUp">
+      <h1 className="signUpHeading">SignUp</h1>
+      <input
+        className="inputBox"
+        type="text"
+        name="name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        placeholder="Enter your name"
+      />
+      <input
+        className="inputBox"
+        type="email"
+        name="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="Enter your email"
+      />
+      <input
+        className="inputBox"
+        type="password"
+        name="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Enter your password"
+      />
+      <button onClick={collectData} className="signUpButton">
+        SignUp
+      </button>
+    </div>
+  );
+};
 
 export default SignUp;
