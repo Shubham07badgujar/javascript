78fdfe98446ee5380e7882e7efbc29c90bbe4012
Shubham07badgujar <shubhambadgujari076@gmail.com>
2025-02-03 22:20:14 +0530

    node]

---
+++ chai aur react/E-DASHBOARD/frontend/backend/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const mongoose = require('mongoose');
+const app = express();
+
+const connectDB = async () =>{
+    mongoose.connect('mongodb://127.0.0.1/e-comm');
+    const productSchema = new mongoose.Schema({});
+    const product = mongoose.model('product',productSchema);
+    const data = await product.find();
+    console.log(data);
+    
+}
+
+connectDB();
+
+// app.get("/",(req, resp)=>{
+//     resp.send("app is working...");
+// });
+app.listen(3000);
